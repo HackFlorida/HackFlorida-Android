ca8debc6f58a281a790f58bb0c03bcbed98a7333
andrewsosa001 <andrewsosa001@gmail.com>
2016-11-15 16:52:36 -0500

    Creates basic data models.

--- app/src/main/java/com/hackflorida/api/model/AnnouncementModel.java
+++ app/src/main/java/com/hackflorida/api/model/AnnouncementModel.java
@@ -1,8 +1,33 @@
 package com.hackflorida.api.model;
 
+import java.util.Calendar;
+
 /**
  * Created by andrew on 11/14/16.
  */
 
 public class AnnouncementModel extends BaseModel {
+
+    String title;
+    String content;
+    Calendar time;
+
+    public AnnouncementModel(String title, String content, Calendar time) {
+        this.title = title;
+        this.content = content;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Calendar getTime() {
+        return time;
+    }
+
 }
