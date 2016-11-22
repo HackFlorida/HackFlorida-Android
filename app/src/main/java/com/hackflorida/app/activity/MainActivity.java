b13edc49cb85ef7dc1c3c37c724c7a354d76948c
andrewsosa001 <andrewsosa001@gmail.com>
2016-11-22 11:17:09 -0500

    Cleans Bottom Nav configuration.

--- app/src/main/java/com/hackflorida/app/activity/MainActivity.java
+++ app/src/main/java/com/hackflorida/app/activity/MainActivity.java
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         mBottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
-
         // Create items
         AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.bbar_announcements, R.drawable.ic_dashboard_black_24dp, R.color.hf_red);
         AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.bbar_schedule, R.drawable.ic_clock_black_24dp, R.color.hf_orange);
@@ -55,42 +54,20 @@ public class MainActivity extends AppCompatActivity {
         mBottomNavigation.addItem(item3);
         mBottomNavigation.addItem(item4);
 
-        // Set background color
-//        mBottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
-
-        // Disable the translation inside the CoordinatorLayout
-//        mBottomNavigation.setBehaviorTranslationEnabled(false);
-
-        // Enable the translation of the FloatingActionButton
-//        mBottomNavigation.manageFloatingActionButtonBehavior(floatingActionButton);
-
-        // Change colors
-//        mBottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
-//        mBottomNavigation.setInactiveColor(Color.parseColor("#747474"));
-
-
         // Force to tint the drawable (useful for font with icon for example)
         mBottomNavigation.setForceTint(true);
 
         // Display color under navigation bar (API 21+)
-        // Don't forget these lines in your style-v21
-        // <item name="android:windowTranslucentNavigation">true</item>
-        // <item name="android:fitsSystemWindows">true</item>
         mBottomNavigation.setTranslucentNavigationEnabled(true);
 
         // Manage titles
         mBottomNavigation.setTitleState(AHBottomNavigation.TitleState.SHOW_WHEN_ACTIVE);
-//        mBottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_SHOW);
-//        mBottomNavigation.setTitleState(AHBottomNavigation.TitleState.ALWAYS_HIDE);
 
         // Use colored navigation with circle reveal effect
         mBottomNavigation.setColored(true);
 
         // Set current item programmatically
         mBottomNavigation.setCurrentItem(1);
-
-        // Customize notification (title, background, typeface)
-//        mBottomNavigation.setNotificationBackgroundColor(Color.parseColor("#F63D2B"));
 
         // Set listeners
         mBottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
@@ -100,12 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-//        mBottomNavigation.setOnNavigationPositionListener(new AHBottomNavigation.OnNavigationPositionListener() {
-//            @Override public void onPositionChange(int y) {
-//                // Manage the new y position
-//            }
-//        });
 
     }
 }
